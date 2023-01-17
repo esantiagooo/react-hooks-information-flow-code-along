bc78971c09582de358621020d0c3e4728f7cf13d
Eric <eric.yanez50@qmail.cuny.edu>
2023-01-17 12:52:02 -0500

    done

--- src/Child.js
+++ src/Child.js
@@ -6,7 +6,9 @@ function Child({ onChangeColor, color }) {
   const handleClick = () =>{
     const newColor = getRandomColor()
     onChangeColor(newColor)
-    // listens for when the function is clicked on and generates a new color at random
+    // onChangeColor is invoked when the function is clicked on, which renders a random color.
+    // It is also used to point back to the parent component which references back to our handleChangeColor function.
+    // which serves as a way to change both the parent and child components. 
   }
   return <div 
   onClick={handleClick}
